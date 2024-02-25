912fe0e23545918fae44fdfc5dd09fac6ea30700
Jenina <jeninadelacruz521@yahoo.com>
2024-02-25 00:51:27 -0800

    added function to start button

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 timerEl = document.querySelector("#timer");
 startQ = document.querySelector(".start");
 time = document.querySelector(".time")
+firstArticle = document.querySelector("article");
+secondPage = document.getElementById("#questions");
+var startQ = document.querySelector(".start");
+
 startQ.addEventListener("click", function()
 {
-    secLeft = 5;
+    secLeft = 10;
     timerEl.textContent = secLeft;
     
     secInterval = setInterval(function(){
@@ -16,18 +20,24 @@ startQ.addEventListener("click", function()
         } 
         
     }, 1000)
+    firstArticle.classList.add("hide");
+    secondPage.classList.remove("hide");
 })
 
 //define the questions and answers
-var startQ = document.querySelector(".start");
+
 var question1 = "What is the capital of USA?";
 var question2 = "What is the capital of UAE?";
 var question3 = "What is the capital of Philippines";
 
 var choices1 = ["A. U", "B. S", "C. A", "D.Washington"];
 var choices2 = ["A. U", "B. A", "C. E", "D.Washington"];
-var choices3 = ["A. P", "B. H", "C. L", "D.Washington"]
+var choices3 = ["A. P", "B. H", "C. L", "D.Washington"];
+var QnA = document.querySelector(".QandA");
 
+function startQuiz(){
+
+}
 //function after clicking start
 
 
