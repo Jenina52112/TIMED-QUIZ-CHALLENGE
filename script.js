575159ce41f22d9e6929d32b835832e1a5b8d32e
Jenina <jeninadelacruz521@yahoo.com>
2024-02-29 04:25:18 -0800

    added functions to javascript

--- script.js
+++ script.js
@@ -1,67 +1,28 @@
+body = document.body;
 timerEl = document.querySelector("#timer");
 startQ = document.querySelector(".start");
 time = document.querySelector(".time")
 headerEl = document.querySelector("header");
 firstArticle = document.querySelector("article");
 secondPage = document.getElementById("questions");
-chooseAnswer = document.querySelector(".answer")
+
 thirdPage = document.getElementById("score-initial");
 scoreInitial = document.getElementById("submitInitial");
 fourthPage = document.getElementById("lastpage");
 var startQ = document.querySelector(".start");
 tryAgain = document.getElementById("retryBtn");
+questionText = document.getElementById("questiontext");
+optionList =  document.querySelector(".choices");
+wrongCorrect = document.querySelector(".wrongcorrect")
+index = 0;
+sequencenum = 0;
+que_num = 1;
+que_count = 0;
 
-
-startQ.addEventListener("click", function()
-{
-    secLeft = 10;
-    timerEl.textContent = secLeft;
-    
-    secInterval = setInterval(function(){
-        if (secLeft > 0){
-            secLeft --;
-            timerEl.textContent = secLeft 
-        }
-        if (secLeft === 0){
-            time.textContent = "Time's Up!"
-        } 
-        
-    }, 1000)
-    firstArticle.classList.add("hide");
-    secondPage.classList.remove("hide");
-
-})
-
-//TODO
-//replace the set of questions and ansers
-
-chooseAnswer.addEventListener("click", function()
-{
-    headerEl.classList.add("hide");
-    firstArticle.classList.add("hide");
-    secondPage.classList.add("hide");
-    thirdPage.classList.remove("hide");
-})
-
-scoreInitial.addEventListener("click", function()
-{
-    headerEl.classList.add("hide");
-    firstArticle.classList.add("hide");
-    secondPage.classList.add("hide");
-    thirdPage.classList.add("hide");
-    fourthPage.classList.remove("hide");
-})
-
-tryAgain.addEventListener("click", function(){
-
-        
-})
-//TODO
-//Take user back to the first page
-
-
-
-//define the questions and answers
+// chooseAnswer = document.querySelector(".answertext");
+// chooseAnswer1 = document.querySelector(".answertext1");
+// chooseAnswer2 = document.querySelector(".answertext2");
+// chooseAnswer3 = document.querySelector(".answertext3");
 var questionList =
     [{
         sequence: 1,
@@ -99,21 +60,92 @@ var questionList =
     },
 ]
 
-
-// var question1 = "What is the capital of USA?";
-// var question2 = "What is the capital of UAE?";
-// var question3 = "What is the capital of Philippines";
-
-// var choices1 = ["A. U", "B. S", "C. A", "D.Washington"];
-// var choices2 = ["A. U", "B. A", "C. E", "D.Washington"];
-// var choices3 = ["A. P", "B. H", "C. L", "D.Washington"];
-// var QnA = document.querySelector(".QandA");
+ console.log(questionList[0].choices);
+ console.log(questionList[0].ask);
+ 
 
 
+startQ.addEventListener("click", function(option_tag)
+{   
+    firstArticle.classList.add("hide");
+    secondPage.classList.remove("hide");
+    secLeft = 10;
+    timerEl.textContent = secLeft;
+    secInterval = setInterval(function(){
+        if (secLeft > 0){
+            secLeft --;
+            timerEl.textContent = secLeft 
+        }
+        if (secLeft === 0){
+            time.textContent = "Time's Up!"
+        } 
+        else if(secLeft === 1){
+            time.textContent = "Time Remaining: " + secLeft + " second"
+        }
+    }, 1000)
+    startQuestions()
+})
+    
+function startQuestions(){
+    que_tag = questionList[index].ask;
+    questionText.textContent = que_tag;
+   
+    option_tag = '<button class = "button">' +  questionList[index].choices[0] + '</button>'+
+    '<button class = "button">' +  questionList[index].choices[1] + '</button>' +
+    '<button class = "button">' +  questionList[index].choices[2] + '</button>'+
+    '<button class = "button">' +  questionList[index].choices[3] + '</button>'
+    optionList.innerHTML = option_tag;
+    
+    choicesbutton = document.querySelectorAll(".button")
+    for(var i = 0; i < choicesbutton.length; i++ ){
+        choicesbutton[i].setAttribute("onclick", "optionSelected(this)")
+           
+        }
 
-// function startQuiz(){
+    }
 
-// }
-//function after clicking start
+    function nextQuestion(){
+
+    }
+
+function optionSelected(answer){
+    var userSelect = answer.textContent;
+    var correctAns = questionList[sequencenum].answer;
+    var allOptions = optionList.children.length;
+
+    if (userSelect == correctAns){
+        wrongCorrect.textContent = "That's Correct!!!"
+    }
+    if (userSelect !== correctAns){
+        wrongCorrect.textContent = "Wrong Answer!!!"
+        secLeft = secLeft - 3
+    }
+
+}
+
+//Last page
+scoreInitial.addEventListener("click", function()
+{
+    headerEl.classList.add("hide");
+    firstArticle.classList.add("hide");
+    secondPage.classList.add("hide");
+    thirdPage.classList.add("hide");
+    fourthPage.classList.remove("hide");
+})
+
+
+//removed form from retry button
+ tryAgain.addEventListener("click", function(){
+    window.location.reload();
+})
+//TODO
+//Take user back to the first page
+
+
+
+
+
+
+// TODO: Set local storage
 
 
