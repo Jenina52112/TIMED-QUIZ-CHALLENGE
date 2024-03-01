60faa986fe81df62c354588d89650390a0b6a756
Jenina <jeninadelacruz521@yahoo.com>
2024-03-01 02:38:18 -0800

    added loop functions to questions

--- script.js
+++ script.js
@@ -5,24 +5,42 @@ time = document.querySelector(".time")
 headerEl = document.querySelector("header");
 firstArticle = document.querySelector("article");
 secondPage = document.getElementById("questions");
-
 thirdPage = document.getElementById("score-initial");
 scoreInitial = document.getElementById("submitInitial");
 fourthPage = document.getElementById("lastpage");
-var startQ = document.querySelector(".start");
+startQ = document.querySelector(".start");
 tryAgain = document.getElementById("retryBtn");
 questionText = document.getElementById("questiontext");
 optionList =  document.querySelector(".choices");
 wrongCorrect = document.querySelector(".wrongcorrect")
 index = 0;
 sequencenum = 0;
-que_num = 1;
-que_count = 0;
 
-// chooseAnswer = document.querySelector(".answertext");
-// chooseAnswer1 = document.querySelector(".answertext1");
-// chooseAnswer2 = document.querySelector(".answertext2");
-// chooseAnswer3 = document.querySelector(".answertext3");
+// questionList = ["What Are the Looping Structures in JavaScript?",
+// "The FUNCTION and VAR are known as:",
+// "What are the different types of Pop up boxes available in JavaScript??",
+// "What was the original name of JavaScript when it discovered?",
+// "Which of the following is not a JavaScript Data Types?"
+// ]
+// //var question1 = "What Are the Looping Structures in JavaScript?";
+// var choices1 = ["For loops", "While loops", "Do-while loops", "All of the above"];
+// answer1 = "All of the above"
+
+// //var question2 = "The FUNCTION and VAR are known as:";
+// var choices2 = ["Declaration statements", "Keywords", "Data types", "Prototypes"]
+// answer2 = "Declaration statements"
+
+// //question3 = "What are the different types of Pop up boxes available in JavaScript??";
+// choices3= ["Alert", "Prompt", "Confirm", "All of the above"]
+// answer3 = "All of the above"
+
+// //question4 = "What was the original name of JavaScript when it discovered?"
+// choices4 = ["LiveScript", "Mocha", "EScript", "JScript"]
+// answer4 = "Mocha"
+
+// //question5 = "Which of the following is not a JavaScript Data Types?"
+// choices5 = ["Boolean", "Undefined", "Float", "Number"]
+// answer5 = "Float"
 var questionList =
     [{
         sequence: 1,
@@ -61,15 +79,20 @@ var questionList =
 ]
 
  console.log(questionList[0].choices);
- console.log(questionList[0].ask);
- 
+ console.log(questionList[2].ask);
+
+
+sequenceQ = questionList[index].sequence;
+for (var i = 0; i <= sequenceQ; i++){
+    sequenceQ = i
+}
 
 
 startQ.addEventListener("click", function(option_tag)
 {   
     firstArticle.classList.add("hide");
     secondPage.classList.remove("hide");
-    secLeft = 10;
+    secLeft = 15;
     timerEl.textContent = secLeft;
     secInterval = setInterval(function(){
         if (secLeft > 0){
@@ -85,7 +108,41 @@ startQ.addEventListener("click", function(option_tag)
     }, 1000)
     startQuestions()
 })
+
+// function startQuestions(){
+//     //que_tag = questionList[index].ask;
+//     for (var i = 0; i < questionList.length; i++){
+//     questionText.textContent = questionList[i];
+//     }
+//     option_tag = '<button class = "button">' + choices1[0] + '</button>'+
+//     '<button class = "button">' +  choices1[1] + '</button>' +
+//     '<button class = "button">' +  choices1[2] + '</button>'+
+//     '<button class = "button">' +  choices1[3] + '</button>'
+//     optionList.innerHTML = option_tag;
     
+//     choicesbutton = document.querySelectorAll(".button")
+//     for(var i = 0; i < choicesbutton.length; i++ ){
+//         choicesbutton[i].setAttribute("onclick", "optionSelected(this)")
+//         }
+    
+// }
+// function optionSelected(answer){
+//         var userSelect = answer.textContent;
+//         var correctAns = answer1;
+//         var allOptions = optionList.children.length;
+        
+//         if (userSelect == correctAns){
+//             wrongCorrect.textContent = "That's Correct!!!"
+//         }
+//         if (userSelect !== correctAns){
+//             wrongCorrect.textContent = "Wrong Answer!!!"
+//             secLeft = secLeft - 3
+//         }
+        
+//         nextQuestion()       
+    
+//     }
+//THIS IS THE PREVIOUS TRY
 function startQuestions(){
     que_tag = questionList[index].ask;
     questionText.textContent = que_tag;
@@ -99,20 +156,15 @@ function startQuestions(){
     choicesbutton = document.querySelectorAll(".button")
     for(var i = 0; i < choicesbutton.length; i++ ){
         choicesbutton[i].setAttribute("onclick", "optionSelected(this)")
-           
         }
-
-    }
-
-    function nextQuestion(){
-
-    }
-
+    
+}
+//THIS IS THE PREVIOUS TRY
 function optionSelected(answer){
     var userSelect = answer.textContent;
     var correctAns = questionList[sequencenum].answer;
     var allOptions = optionList.children.length;
-
+    
     if (userSelect == correctAns){
         wrongCorrect.textContent = "That's Correct!!!"
     }
@@ -120,8 +172,31 @@ function optionSelected(answer){
         wrongCorrect.textContent = "Wrong Answer!!!"
         secLeft = secLeft - 3
     }
+    
+    nextQuestion()       
 
 }
+//THIS IS THE PREVIOUS TRY
+function nextQuestion(){
+    for (var i = 0; i < 5; i++){
+        index = i
+    }
+   
+    startQuestions()
+    
+}
+// }
+// for (var index = 0; index < 5; index++){
+//     option_tag = '<button class = "button">' +  questionList[index].choices[0] + '</button>'+
+// '<button class = "button">' +  questionList[index].choices[1] + '</button>' +
+// '<button class = "button">' +  questionList[index].choices[2] + '</button>'+
+// '<button class = "button">' +  questionList[index].choices[3] + '</button>'
+// optionList.innerHTML = option_tag;
+     
+// choicesbutton = document.querySelectorAll(".button")
+// for(var i = 0; i < choicesbutton.length; i++ ){
+//     choicesbutton[i].setAttribute("onclick", "optionSelected(this)")
+//     }
 
 //Last page
 scoreInitial.addEventListener("click", function()
@@ -134,18 +209,16 @@ scoreInitial.addEventListener("click", function()
 })
 
 
-//removed form from retry button
- tryAgain.addEventListener("click", function(){
-    window.location.reload();
-})
+
+ 
 //TODO
 //Take user back to the first page
 
 
-
+tryAgain.addEventListener("click", function(){
+    window.location.reload();
+})
 
 
 
 // TODO: Set local storage
-
-
